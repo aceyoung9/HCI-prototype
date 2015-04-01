e9285b213a5407f9a30485a7dda8c6e31b730033
Molly White <molly.white5@gmail.com>
2015-04-01 18:04:02 -0400

    Use users endpoint in prototype

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,23 @@
 $(function() {
   var channels_source = $("#channels-template").html();
+  var users_source = $("#users-template").html();
+  
   var channels_template = Handlebars.compile(channels_source);
+  var users_template = Handlebars.compile(users_source);
+
   var channels_promise = $.getJSON('http://localhost:4730/channels')
-  channels_promise.done( function(data) {
-    var channels_html = channels_template(data);
+
+  channels_promise.done( function(channel_data) {
+    var channels_html = channels_template(channel_data);
     $("section.channels-column ul").append(channels_html);
+
+    var users_promise = $.getJSON('http://localhost:4730/users/' +
+      encodeURIComponent(channel_data[0]) );
+    
+    users_promise.done( function(user_data) {
+      var users_html = users_template(user_data);
+      $("section.users-column ul").append(users_html);
+    })
+
   })
 });
