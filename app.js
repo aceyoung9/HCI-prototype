ab5a2555054b0216a68bc1e9076e25302fd33f8b
Molly White <molly.white5@gmail.com>
2015-04-03 21:36:07 -0400

    Relative times

--- app.js
+++ app.js
@@ -11,66 +11,66 @@ var data = {
     activity: [
     {
       user: "somealumni",
-      timestamp: moment('02-04-2015 10:00:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(10, 'minutes'),
       type: "message",
       content: "Bushwick gentrify fixie, normcore scenester mixtape fingerstache"
     },
     {
       user: "molly",
-      timestamp: moment('02 Apr 2015 10:01:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(9, 'minutes'),
       type: "message",
       content: "3 wolf moon bitters plaid Intelligentsia cold-pressed Neutra sriracha stumptown"
     },
     {
       user: "alice",
-      timestamp: moment('02 Apr 2015 10:01:30', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(9, 'minutes'),
       type: "message",
       content: "single-origin coffee PBR cornhole small batch stumptown ennui"
     },
     {
       user: "alice",
-      timestamp: moment('02 Apr 2015 10:02:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(8, 'minutes'),
       type: "message",
       content: "church-key locavore four loko 90's Carles Pitchfork"
     },
     {
       user: "molly",
-      timestamp: moment('02 Apr 2015 10:03:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(7, 'minutes'),
       type: "message",
       content: "pop-up deep v bespoke, master cleanse keffiyeh organic heirloom"
     },
     {
       user: "somealumni",
-      timestamp: moment('02 Apr 2015 10:03:25', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(7, 'minutes'),
       type: "message",
       content: "Four loko Blue Bottle paleo, quinoa post-ironic Shoreditch banh mi kitsch VHS pickled hashtag church-key typewriter pug cred. BR Wes Anderson letterpress disrupt, pork belly master cleanse bicycle rights selfies wayfarers tilde chia umami. Tilde chillwave YOLO direct trade sriracha fanny pack. Master cleanse polaroid roof party, single-origin coffee distillery photo booth tattooed swag banh mi. Health goth Pitchfork hashtag, sustainable hella small batch paleo pour-over craft beer American Apparel narwhal trust fund mlkshk jean shorts distillery."
     },
     {
       user: "alice",
-      timestamp: moment('02 Apr 2015 10:04:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(3, 'minutes'),
       type: "message",
       content: "Portland wayfarers Carles meggings you probably haven't heard of them"
     },
     {
       user: "molly",
-      timestamp: moment('02 Apr 2015 10:10:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(3, 'minutes'),
       type: "message",
       content: " irony readymade pop-up master cleanse typewriter migas listicle"
     },
     {
       user: "molly",
-      timestamp: moment('02 Apr 2015 10:10:30', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(3, 'minutes'),
       type: "message",
       content: "crucifix PBR four dollar toast, hoodie chia authentic pug asymmetrical roof party vegan small batch"
     },
     {
       user: "sarah",
-      timestamp: moment('02 Apr 2015 10:15:30', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(2, 'minutes'),
       type: "join"
     },
     {
       user: "molly",
-      timestamp: moment('02 Apr 2015 10:16:00', 'DD-MM-YYYY HH:mm:ss'),
+      timestamp: moment().subtract(1, 'minutes'),
       type: "message",
       content: "Bicycle rights Bushwick chambray Schlitz vinyl, VHS selvage before they sold out paleo viral."
     }
